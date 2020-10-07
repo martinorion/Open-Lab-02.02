500ee449b8f03a921f5dcb0914c14a3803d2c8d0
martinorion <71751917+martinorion@users.noreply.github.com>
2020-10-07 16:44:50 +0200

    Update Formatter.cs

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_02._02
 {
@@ -6,7 +6,12 @@ namespace Open_Lab_02._02
     {
         public void Format(double[] numbers)
         {
-            throw new NotImplementedException();
-        }
+            string output = "";
+            foreach(double num in numbers)
+            { output += Math.Round(num, 2) + "";
+            }
+            Console.Write(output.Substring(0, output.Length - 1));
     }
+
+}
 }
